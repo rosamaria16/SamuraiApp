f4d7c49d685d17e5e8bdbe24786422acf0838cf8
Rosa María Espinosa Martínez <r.espinosa.martinez@avanade.com>
2025-11-05 09:16:02 +0100

    add: Clan, Quote and Samurai classes

---
+++ SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,15 @@
+﻿namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public Samurai() 
+        {
+            Quotes = new List<Quote>();
+        }
+
+        public int Id { get; set; }
+        public Clan Clan { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+    }
+}
